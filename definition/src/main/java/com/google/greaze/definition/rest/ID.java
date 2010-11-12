d18cf2b826717e253f820c3f53bd2fa29aaf1055
Inderjeet Singh <inder123@gmail.com>
2010-11-12 09:08:39 -0800

    Initial creation of the greaze-definition project by moving the contents from gson/wsdef

---
+++ definition/src/main/java/com/google/greaze/definition/rest/ID.java
@@ -0,0 +1,7 @@
+package com.google.greaze.definition.rest;
+
+public interface ID {
+  public static final long INVALID_ID = -1L;
+
+  public long getValue();
+}
---
+++ definition/src/main/java/com/google/greaze/definition/rest/query/TypedKeysQuery.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.greaze.definition.rest.query;
+
+import java.util.List;
+
+import com.google.greaze.definition.TypedKey;
+import com.google.greaze.definition.rest.ID;
+import com.google.greaze.definition.rest.RestResource;
+
+/**
+ * List of {@link TypedKey}s associated with REST queries
+ *
+ * @author Inderjeet Singh
+ *
+ * @param <I> ID type of the REST resource
+ * @param <R> The type of the REST resource
+ */
+@SuppressWarnings("rawtypes")
+public class TypedKeysQuery<I extends ID, R extends RestResource<I, R>> {
+
+  // TODO(inder): This should really be TypedKey<List<R>> 
+  public static TypedKey<List> RESOURCE_LIST = new TypedKey<List>("resourceList", List.class);
+}
