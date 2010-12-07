3ac79a31d8e7dbc2a0d28de539b997da52db69ff
inder123 <inder123@gmail.com>
2010-12-07 09:33:54 -0800

    Added fixtures for end-to-end testing. Started adding of query parameter testing. Started a NetworkSwitcher that can pipe input to output.

---
+++ end2end/src/test/java/com/google/greaze/end2end/query/WebServiceClientFake.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2008 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.greaze.end2end.query;
+
+import com.google.greaze.definition.fixtures.NetworkSwitcher;
+import com.google.greaze.definition.fixtures.NetworkSwitcherPiped;
+import com.google.greaze.webservice.client.ServerConfig;
+import com.google.greaze.webservice.client.WebServiceClient;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * A test fixture for {@link WebServiceClient}
+ *
+ * @author Inderjeet Singh
+ */
+public class WebServiceClientFake extends WebServiceClient {
+
+  private final NetworkSwitcher networkSwitcher;
+  public WebServiceClientFake() {
+    super(new ServerConfig("http://localhost"));
+    networkSwitcher = new NetworkSwitcherPiped();
+  }
+
+  @Override
+  protected HttpURLConnection openConnection(URL url) {
+    return networkSwitcher.get(url);
+  }
+}
