61da4c506f6ad5c1c7a9384ba1b0f8674b61add7
inder123 <inder123@gmail.com>
2010-12-08 13:02:17 -0800

    Moved HttpURLConnectionFake as an inner class for NetworkSwitcherPiped.

--- definition/src/test/java/com/google/greaze/definition/fixtures/NetworkSwitcherPiped.java
+++ definition/src/test/java/com/google/greaze/definition/fixtures/NetworkSwitcherPiped.java
@@ -15,6 +15,12 @@
  */
 package com.google.greaze.definition.fixtures;
 
+import com.google.greaze.definition.ContentBodySpec;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -29,4 +35,46 @@ public class NetworkSwitcherPiped implements NetworkSwitcher {
   public HttpURLConnection get(URL url) {
     return new HttpURLConnectionFake(url);
   }
+
+  protected void switchNetwork(HttpURLConnectionFake conn) {
+  }
+
+  public final class HttpURLConnectionFake extends HttpURLConnection {
+
+    private final ByteArrayOutputStream out;
+    public HttpURLConnectionFake(URL url) {
+      super(url);
+      out = new ByteArrayOutputStream();
+    }
+
+    @Override
+    public void disconnect() {
+    }
+
+    @Override
+    public boolean usingProxy() {
+      return false;
+    }
+
+    @Override
+    public String getContentType() {
+      return ContentBodySpec.JSON_CONTENT_TYPE;
+    }
+    
+    @Override
+    public InputStream getInputStream() {
+      return new ByteArrayInputStream(out.toByteArray());
+    }
+
+    @Override
+    public OutputStream getOutputStream() {
+      return out;
+    }
+
+    @Override
+    public void connect() {
+      switchNetwork(this);
+    }
+  }
 }
+
