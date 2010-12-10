7491bffd7e5b944508e1a2e6be7bed4b9a51b03a
inder123 <inder123@gmail.com>
2010-12-10 12:07:34 -0800

    moved to a more appropriate package

--- end2end/src/test/java/com/google/greaze/end2end/resources/ResourceDepotFunctionalTest.java
+++ end2end/src/test/java/com/google/greaze/end2end/resources/ResourceDepotFunctionalTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.greaze.end2end.query;
+package com.google.greaze.end2end.resources;
 
 
 import com.google.greaze.definition.CallPath;
