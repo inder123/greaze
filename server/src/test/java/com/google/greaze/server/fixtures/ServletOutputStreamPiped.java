8594261caf0e49e7043e3d092dac02b676b21e80
inder123 <inder123@gmail.com>
2010-12-09 16:01:15 -0800

    Created a piped ServetOutputStream that outputs all data to another output stream.

---
+++ server/src/test/java/com/google/greaze/server/fixtures/ServletOutputStreamPiped.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.greaze.server.fixtures;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.ServletOutputStream;
+
+/**
+ * A {@link ServletOutputStream} that serves from a {@link OutputStream}
+ *
+ * @author Inderjeet Singh
+ */
+public class ServletOutputStreamPiped extends ServletOutputStream {
+  private final OutputStream out;
+  public ServletOutputStreamPiped(OutputStream out) {
+    this.out = out;
+  }
+  @Override
+  public void write(int b) throws IOException {
+    out.write(b);
+  }
+  @Override
+  public void write(byte b[]) throws IOException {
+    out.write(b);
+  }
+  @Override
+  public void write(byte b[], int off, int len) throws IOException {
+    out.write(b, off, len);
+  }
+  @Override
+  public void flush() throws IOException {
+    out.flush();
+  }
+  @Override
+  public void close() throws IOException {
+    out.close();
+  }
+}
