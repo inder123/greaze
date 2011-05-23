be628e0c35961e4855b0322b941a537139746383
Inderjeet Singh <inder123@gmail.com>
2011-05-23 00:48:20 -0700

    Revised MetaDataMapBase to use id value as the key in the map. Added its toString() method. removed unnecessary unused warnings from WebServiceCallServerBuilder.

--- server/src/main/java/com/google/greaze/rest/server/MetaDataMapBase.java
+++ server/src/main/java/com/google/greaze/rest/server/MetaDataMapBase.java
@@ -15,12 +15,9 @@
  */
 package com.google.greaze.rest.server;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import com.google.greaze.definition.TypedKey;
 import com.google.greaze.definition.rest.MetaDataBase;
 import com.google.greaze.definition.rest.ResourceId;
 import com.google.greaze.definition.rest.RestResourceBase;
@@ -35,66 +32,28 @@ import com.google.greaze.definition.rest.RestResourceBase;
  * @param <R> the rest resource for which the metadata is being stored
  */
 public class MetaDataMapBase<I extends ResourceId, R extends RestResourceBase<I, R>> {
-  protected final Map<I, MetaDataBase<I, R>> map;
+  protected final Map<String, MetaDataBase<I, R>> map;
 
   public MetaDataMapBase() {
-    this.map = new HashMap<I, MetaDataBase<I, R>>();
+    this.map = new HashMap<String, MetaDataBase<I, R>>();
   }
 
   public MetaDataBase<I, R> get(I resourceId) {
     MetaDataBase<I, R> metaData = map.get(resourceId);
     if (metaData == null) {
       metaData = createMetaData();
-      map.put(resourceId, metaData);
+      map.put(resourceId.getValue(), metaData);
     }
     return metaData;
   }
 
   public void remove(I resourceId) {
-    map.remove(resourceId);
+    map.remove(resourceId.getValue());
   }
 
   @Override
   public String toString() {
     return String.format("%s", map);
-  }
-
-  /**
-   * Returns a list of Ids that match the specified value excluding idToSkip
-   *
-   * @param <T> Type of the specified key
-   * @param key The key used to retrieve value from transient store
-   * @param value Only those ids are returned that have value same as this one
-   * @param maxCount The maximum number of values to return.
-   * @param idToSkip The returned list should not contain the specified id.
-   * @return a list of Ids that match the specified value excluding idToSkip 
-   */
-  public <T> List<I> findByTypedKey(TypedKey<T> key, T value, int maxCount, I idToSkip) {
-    List<I> result = new ArrayList<I>();
-    for (Map.Entry<I, MetaDataBase<I, R>> entry : map.entrySet()) {
-      I id = entry.getKey();
-      if (id.equalsByValue(idToSkip)) {
-        continue;
-      }
-      T retrieved = entry.getValue().getFromTransient(key);
-      if (isEqual(value, retrieved)) {
-        result.add(id);
-        if (--maxCount == 0) {
-          break;
-        }
-      }
-    }
-    return result;
-  }
-
-  public static <T> boolean isEqual(T t1, T t2) {
-    if (t1 == null && t2 == null) {
-      return true;
-    }
-    if (t1 == null || t2 == null) {
-      return false;
-    }
-    return t1.equals(t2);
   }
 
   /**
