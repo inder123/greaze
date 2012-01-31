41639667cbcfc8926125d0d74729fd38ff259108
inder123 <inder123@localhost>
2012-01-31 00:28:09 -0800

    revised to exclude static and transient fields.

--- definition/src/main/java/com/google/greaze/definition/internal/utils/FieldNavigator.java
+++ definition/src/main/java/com/google/greaze/definition/internal/utils/FieldNavigator.java
@@ -17,6 +17,7 @@ package com.google.greaze.definition.internal.utils;
 
 import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
@@ -49,7 +50,10 @@ public final class FieldNavigator {
     Field[] fields = clazz.getDeclaredFields();
     AccessibleObject.setAccessible(fields, true);
     for (Field f : fields) {
-      fieldsList.add(f);
+      int modifiers = f.getModifiers();
+      if (!f.isSynthetic() && !Modifier.isTransient(modifiers) && !Modifier.isStatic(modifiers)) {
+        fieldsList.add(f);
+      }
     }
   }
 }
